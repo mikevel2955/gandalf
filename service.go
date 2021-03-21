758e30bba1fb0d55563dab27cd9cf68b9010b3b8
BeJloMux <michael-velmiskin@yandex.ru>
2021-03-22 00:00:56 +0400

    initial commit 05

--- service.go
+++ service.go
@@ -7,6 +7,7 @@ import (
 
 	pb "github.com/mikevel2955/gandalf/pb"
 	"go.uber.org/zap"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type Server struct {
@@ -16,17 +17,25 @@ type Server struct {
 
 	// Mock fields
 	symbols []tradingSymbol
+	deals   []*pb.Deal
 }
 
 type tradingSymbol struct {
 	symbol  string
 	status  pb.TradingSymbol_TradingStatus
 	balance float32
+	limit   float32
 }
 
 var (
 	errUserNotOperator = errors.New("you are not authorized to perform this operation")
 	errUserNotViewer   = errors.New("you are not authorized to view this data")
+	errSymbolNotFound  = func(symbol string) error {
+		return errors.New(fmt.Sprintf("unknown symbol '%s'", symbol))
+	}
+	errDealNotFound = func(dealId string) error {
+		return errors.New(fmt.Sprintf("unknown deal '%s'", dealId))
+	}
 )
 
 func NewServer(
@@ -40,11 +49,48 @@ func NewServer(
 		userViewers:   userViewers,
 
 		symbols: []tradingSymbol{
-			{"adausdt", pb.TradingSymbol_ACTIVE, 55},
-			{"linkusdt", pb.TradingSymbol_ACTIVE, 66},
-			{"zilusdt", pb.TradingSymbol_ACTIVE, 33},
-			{"ltcusdt", pb.TradingSymbol_ACTIVE, 22},
+			{"adausdt", pb.TradingSymbol_ACTIVE, 55, 100},
+			{"linkusdt", pb.TradingSymbol_ACTIVE, 66, 100},
+			{"zilusdt", pb.TradingSymbol_ACTIVE, 33, 100},
+			{"ltcusdt", pb.TradingSymbol_ACTIVE, 22, 100},
 		},
+		deals: []*pb.Deal{{
+			DealId:         "adausdt-1657483456",
+			Symbol:         "adausdt",
+			CreatedAt:      timestamppb.Now(),
+			Amount:         0.01,
+			AmountCurrency: 361,
+			DeltaAmount:    -12,
+			DeltaPercent:   -2,
+			Prediction: &pb.Deal_DealPrediction{
+				Stop: -3,
+				Max:  2,
+			},
+		}, {
+			DealId:         "adausdt-1630958723",
+			Symbol:         "adausdt",
+			CreatedAt:      timestamppb.Now(),
+			Amount:         0.04,
+			AmountCurrency: 734,
+			DeltaAmount:    15,
+			DeltaPercent:   2,
+			Prediction: &pb.Deal_DealPrediction{
+				Stop: -5,
+				Max:  7,
+			},
+		}, {
+			DealId:         "linkusdt-1630958723",
+			Symbol:         "linkusdt",
+			CreatedAt:      timestamppb.Now(),
+			Amount:         0.5,
+			AmountCurrency: 154,
+			DeltaAmount:    7,
+			DeltaPercent:   5,
+			Prediction: &pb.Deal_DealPrediction{
+				Stop: -15,
+				Max:  7,
+			},
+		}},
 	}
 }
 
@@ -88,7 +134,7 @@ func (s *Server) SymbolTradingStart(_ context.Context, req *pb.SymbolRequest) (*
 		return nil, err
 	}
 
-	panic("implement me")
+	return s.setSymbolStatus(req.Symbol, pb.TradingSymbol_ACTIVE)
 }
 
 func (s *Server) SymbolTradingStop(_ context.Context, req *pb.SymbolRequest) (*pb.EmptyResponse, error) {
@@ -96,7 +142,14 @@ func (s *Server) SymbolTradingStop(_ context.Context, req *pb.SymbolRequest) (*p
 		return nil, err
 	}
 
-	panic("implement me")
+	n, err := s.findSymbolIndex(req.Symbol)
+	if err != nil {
+		return nil, err
+	}
+
+	s.symbols = append(s.symbols[:n], s.symbols[n+1:]...)
+
+	return &pb.EmptyResponse{}, nil
 }
 
 func (s *Server) SymbolTradingSuspend(_ context.Context, req *pb.SymbolRequest) (*pb.EmptyResponse, error) {
@@ -104,7 +157,7 @@ func (s *Server) SymbolTradingSuspend(_ context.Context, req *pb.SymbolRequest) 
 		return nil, err
 	}
 
-	panic("implement me")
+	return s.setSymbolStatus(req.Symbol, pb.TradingSymbol_SUSPENDED)
 }
 
 func (s *Server) SymbolTradingResume(_ context.Context, req *pb.SymbolRequest) (*pb.EmptyResponse, error) {
@@ -112,7 +165,7 @@ func (s *Server) SymbolTradingResume(_ context.Context, req *pb.SymbolRequest) (
 		return nil, err
 	}
 
-	panic("implement me")
+	return s.setSymbolStatus(req.Symbol, pb.TradingSymbol_ACTIVE)
 }
 
 func (s *Server) GetSymbolBalances(_ context.Context, req *pb.EmptyRequest) (*pb.SymbolBalancesResponse, error) {
@@ -121,12 +174,19 @@ func (s *Server) GetSymbolBalances(_ context.Context, req *pb.EmptyRequest) (*pb
 	}
 
 	var balances []*pb.SymbolBalance
+	var total float32
 	for _, symbol := range s.symbols {
 		balances = append(balances, &pb.SymbolBalance{
 			Symbol: symbol.symbol,
 			Amount: symbol.balance,
 		})
+		total += symbol.balance
 	}
+
+	balances = append(balances, &pb.SymbolBalance{
+		Symbol: "usd",
+		Amount: total,
+	})
 
 	return &pb.SymbolBalancesResponse{
 		Balances: balances,
@@ -138,7 +198,17 @@ func (s *Server) GetSymbolLimits(_ context.Context, req *pb.GetSymbolLimitsReque
 		return nil, err
 	}
 
-	panic("implement me")
+	limits := make([]*pb.SymbolLimit, 0, len(s.symbols))
+	for _, symbol := range s.symbols {
+		limits = append(limits, &pb.SymbolLimit{
+			Symbol: symbol.symbol,
+			Limit:  symbol.limit,
+		})
+	}
+
+	return &pb.SymbolLimitsResponse{
+		Limits: limits,
+	}, nil
 }
 
 func (s *Server) SetSymbolLimits(_ context.Context, req *pb.SetSymbolLimitsRequest) (*pb.EmptyResponse, error) {
@@ -146,12 +216,27 @@ func (s *Server) SetSymbolLimits(_ context.Context, req *pb.SetSymbolLimitsReque
 		return nil, err
 	}
 
-	panic("implement me")
+	for _, limit := range req.Limits {
+		n, err := s.findSymbolIndex(limit.Symbol)
+		if err != nil {
+			return nil, err
+		}
+
+		s.symbols[n].limit = limit.Limit
+	}
+
+	return &pb.EmptyResponse{}, nil
 }
 
 func (s *Server) GetActiveDeals(_ context.Context, req *pb.DealsRequest) (*pb.DealsResponse, error) {
 	if err := s.checkUserViewer(req.UserId); err != nil {
 		return nil, err
+	}
+
+	if req.All {
+		return &pb.DealsResponse{
+			Deals: s.deals,
+		}, nil
 	}
 
 	panic("implement me")
@@ -170,7 +255,20 @@ func (s *Server) CloseDeals(_ context.Context, req *pb.DealsRequest) (*pb.EmptyR
 		return nil, err
 	}
 
-	panic("implement me")
+	if req.All {
+		s.deals = []*pb.Deal{}
+		return &pb.EmptyResponse{}, nil
+	}
+
+	for _, dealId := range req.DealIds {
+		n, err := s.findDealIndex(dealId)
+		if err != nil {
+			return nil, err
+		}
+		s.deals = append(s.deals[:n], s.deals[n+1:]...)
+	}
+
+	return &pb.EmptyResponse{}, nil
 }
 
 func (s *Server) checkUserOperator(userId int64) error {
@@ -185,6 +283,35 @@ func (s *Server) checkUserViewer(userId int64) error {
 		return errUserNotViewer
 	}
 	return nil
+}
+
+func (s *Server) setSymbolStatus(symbol string, status pb.TradingSymbol_TradingStatus) (*pb.EmptyResponse, error) {
+	n, err := s.findSymbolIndex(symbol)
+	if err != nil {
+		return nil, err
+	}
+
+	s.symbols[n].status = status
+
+	return &pb.EmptyResponse{}, nil
+}
+
+func (s *Server) findSymbolIndex(symbol string) (int, error) {
+	for i := range s.symbols {
+		if s.symbols[i].symbol == symbol {
+			return i, nil
+		}
+	}
+	return -1, errSymbolNotFound(symbol)
+}
+
+func (s *Server) findDealIndex(dealId string) (int, error) {
+	for n := range s.deals {
+		if s.deals[n].DealId == dealId {
+			return n, nil
+		}
+	}
+	return -1, errDealNotFound(dealId)
 }
 
 func int64InList(n int64, list []int64) bool {
